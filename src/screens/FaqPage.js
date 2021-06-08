712c035ee8d41f6c964a314231c599611b4cdddc
lemankay <46055728+lemankay@users.noreply.github.com>
2021-06-08 11:44:58 +0300

    Update FaqPage.js

--- src/screens/FaqPage.js
+++ src/screens/FaqPage.js
@@ -10,23 +10,7 @@ const Stack = createStackNavigator();
 
 function FaqPage() {
   return (
-    <Stack.Navigator>
-        <Stack.Screen name="Faq" component={Faq}      options={{
-          title: 'Faq',
-          headerStyle: {
-            backgroundColor: '#f3f2f2',
-          },
-          headerTintColor: '#000',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-            marginLeft:'45%' 
-          },
-        }}
-        />
-        <Stack.Screen name="Authorization_Issues" component={Authorization_Issues}  options={{ title: 'Authorization Issues' }} />
-        <Stack.Screen name="The_first_steps" component={The_first_steps} options={{ title: 'The first steps' }}/>
-        <Stack.Screen name="Payment" component={Payment} />
-    </Stack.Navigator>
+
   );
 }
 export default FaqPage;
