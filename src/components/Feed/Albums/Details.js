e2ce7631d2839d27bcb48c11f30ec905c2f6a0d8
lemankay <46055728+lemankay@users.noreply.github.com>
2021-06-08 11:48:09 +0300

    Update Details.js

--- src/components/Feed/Albums/Details.js
+++ src/components/Feed/Albums/Details.js
@@ -6,22 +6,7 @@ export default function Details({ album, setAlbumDetails }) {
   return (
     <View style={{flex:1,flexDirection:"row",  paddingVertical: 15,}} >
       
-           
-            <Image
-            style={{width: 100, height: 100, borderRadius: 10, margin: 10}}
-            source={{uri: album.photos.data[0].url}}
-          />  
-          <View>
-              <Text> {album.title}</Text>  
-              <Text> {album.user.name}</Text>   
-                  <TouchableOpacity onPress={()=> setAlbumDetails(null)}   >
-          <MaterialCommunityIcons
-            name="trash-can-outline"
-            color="black"
-            size={20}
-          />     
-      </TouchableOpacity> 
-          </View>
+   
      
         
           
