b8f0816a8620d5a4c8ba5f9793495ed7fc9e9d41
lemankay <46055728+lemankay@users.noreply.github.com>
2021-06-08 11:54:25 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -25,48 +25,6 @@ const client = new ApolloClient({
 });
 
 
-const Tab = createBottomTabNavigator();
-
-function MyTabs() {
-  return (
-    <Tab.Navigator initialRouteName="Feed" tabBarOptions={{
-        activeTintColor: '#e91e63',
-      }}
-    >
-      <Tab.Screen
-        name="Feed"
-        component={Feed}
-        options={{
-          tabBarLabel: 'Feed',
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="folder-multiple-image" color={color} size={size} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="FaqPage"
-        component={FaqPage}
-        options={{
-          tabBarLabel: 'Faq Page',
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="help-circle-outline" color={color} size={size} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="UserPage"
-        component={UserPage}
-        options={{
-          tabBarLabel: 'User',
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="account" color={color} size={size} />
-          ),
-        }}
-      />
-    </Tab.Navigator>
-  );
-}
-
 
 
 
